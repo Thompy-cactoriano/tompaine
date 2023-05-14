13539666adca4d2d4023fbc3f5af8d45469e50ac
Thompy <thompythompyson@gmail.com>
2023-05-14 18:28:07 -0300

    Voltando ao cmake e pintando o fundo com opengl

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,12 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+		glClearColor(0.3, 0.2, 0.9, 0.1);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-//        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
