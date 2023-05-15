7857d2384bdf09b1941bf9b8f67aa8e0a6a779f4
Thompy <thompythompyson@gmail.com>
2023-05-14 22:55:32 -0300

    Só pra atualizar

--- src/tompaine.hpp
+++ src/tompaine.hpp
@@ -2,4 +2,11 @@
 
 namespace tp{
 	void draw_rect(float x, float y,  float width,  float height, float r, float b, float g);
+
+	class ColorRGB{
+		public:
+		float r, g, b;
+
+		ColorRGB(float r, float g, float b);
+	}
 }
